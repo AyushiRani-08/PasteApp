7f17f85bc90e3abdad202aed5f520972d67196b2
AyushiRani-08 <ayushijsr8@gmail.com>
2025-08-14 01:42:27 +0530

    add

---
+++ Home.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { addToPaste, updateToPaste } from '../redux/pasteSlice';
+
+const Home = () => {
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState('');
+    const [serachParams, setSearchParams] = useSearchParams();
+    const pasteID = serachParams.get("pasteId");
+    const dispatch = useDispatch();
+    const allPaste = useSelector((state) => state.paste.pastes);
+    useEffect(() => {
+        if (pasteID) {
+            const paste = allPaste.find((p) => p._id === pasteID);
+            setTitle(paste.title);
+            setValue(paste.content);
+        }
+
+    }, [pasteID])
+
+    function createPaste() {
+        const paste = {
+            title: title,
+            content: value,
+            _id: pasteID || Date.now().toString(36),
+            createdAt: new Date().toISOString(),
+        }
+
+
+        if (pasteID) {
+            //update paste
+            dispatch(updateToPaste(paste));
+        } else {
+            //create paste 
+            dispatch(addToPaste(paste));
+        }
+        //after creation or updation
+        setTitle('');
+        setValue('');
+        setSearchParams({});
+
+    }
+    return (
+        <div>
+            <div className='flex flex-row gap-7 place-content-between'>
+                <input
+                    className='p-2 rounded-2xl mt-2 w-[67%] pl-4'
+                    type='text'
+                    placeholder='Enter title here'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <button className='p-2 rounded-2xl mt-2' onClick={createPaste}>
+                    {
+                        pasteID ? "Update Paste" : "Create my Paste"
+                    }
+                </button>
+            </div>
+            <div className='mt-8'>
+                <textarea
+                    className='rounded-2xl mt-4 min-w-[500px] p-4'
+                    value={value}
+                    placeholder='Enter Content here'
+                    onChange={(e) => setValue(e.target.value)}
+                    rows={20}
+
+                />
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Home
